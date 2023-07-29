67ce95998855a36b2883aa182ec8b323e97d5b67
anickacodes <anickalewis@pursuit.org>
2023-07-28 23:42:59 -0400

    added dir , updated DOM content , stlyling inline

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import "./App.css";
 function App () {
     return (
       <main>
-        <p>React State Lab</p>
+        <p>Nicki's React State Lab</p>
+{/* <h1>Current Score: {score} </h1> */}
+<input type="button" value="+1" onClick={()=>{console.log("ok")}} style={{border: "2px solid green", color: "purple"}} />
+{/* style={h} */}
+{/* value={count} */}
+
       </main>
     );
 }
