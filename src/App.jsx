ddec2c5904b0ee704faa4a4e5481edfb9d3c6102
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-08-02 05:34:04 -0400

    You Win + Conditionnal Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import React, { useState } from "react";
 import "./App.css";
+import ClickerGame from "./Components/Clicker-Game";
+
 
 function App() {
-  const [score, setScore] = useState(0);
-  const [incrementValue, setIncrementValue] = useState(1);
+
+  
   return (
     <main>
-      <p>React State Lab</p>
-
-      <h1>Current Score: {score}</h1>
-
-      <button onClick={() => score<100 ? setScore(score + incrementValue) : null /* need to write code that display victory state */ }> +{incrementValue}</button>
-      <br />
-      <br />
-      <button onClick={() => {
-        score >= 10 ?(setScore(score - 10), setIncrementValue(incrementValue + 1)) : window.alert("You can't afford that");
-      }
-      }>
-        Pay 10 points to change from +{incrementValue} to +{incrementValue + 1} </button>
+      <p>Let's Play!</p>
+      <div id="game-area">
+      {<ClickerGame />}
+      </div>
     </main>
   );
 }
