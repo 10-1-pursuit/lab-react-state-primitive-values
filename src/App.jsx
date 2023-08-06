1d625579751526ee362ff6233356c23d717f7dd0
Rose <i.a.rose.bk@gmail.com>
2023-08-06 11:17:09 -0400

    Updated current score button

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
+import { useState } from "react";
 import React from "react";
 import "./App.css";
 
-function App () {
-    return (
-      <>
+function App() {
+  const [score,setScore]= useState(0)
+  const [nextLevel,setnextLevel]= useState(1)
+
+  return ( 
+    <>
       <main>
         <h1>
-        {/* React State Lab */}
-        Current Score: 0
-          </h1>
-      <button type={+1} button/>
-      </main>
-      </>
+          {/* React State Lab */}
+          Current Score: {score}
+        </h1>
+        <button onClick={ (se) => {setScore(score+1)} }> +{nextLevel }
+          +1🌹 
+        </button>     
+     </main>
+    </>
 
-    );
+  );
 }
 
 export default App;
